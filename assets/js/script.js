50974f0fb2b63809139fc989bff5d0b0f37681d9
Caroline <Cluce059@gmail.com>
2021-07-05 17:40:04 -0400

    form submission functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,12 @@
 //console.log(buttonEl);
 //add DOM object reference to task list towards the top pf the page
 //then add event listener and change text of new task item dynamically using textContent to identify it as a new task
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //define this b4 event listener bc it wont know about this funciton otherwise
-var createTaskHandler = function(){
+var createTaskHandler = function(event){
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -15,4 +16,4 @@ var createTaskHandler = function(){
 };
 
 //dynamically create li elements in the DOM
-buttonEl.addEventListener("click", createTaskHandler);//"in a click instance, create a task"
+formEl.addEventListener("submit", createTaskHandler);//listedn for an instance of clicking submit or pressing the enter key
